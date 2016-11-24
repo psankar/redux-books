1c3a7a41827d880b2aebc50b6f7a769da19c7d1e
Sankar சங்கர் <sankar.curiosity@gmail.com>
2016-11-24 16:39:08 +0530

    49 Conditional rendering

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function() {
     return [
-        {title: '1'},
-        {title: '2'},
-        {title: '3'},
-        {title: '4'},
-        {title: '5'}
+        {title: '1', pages: 1},
+        {title: '2', pages: 10},
+        {title: '3', pages: 100},
+        {title: '4', pages: 1000},
+        {title: '5', pages: 10000}
     ]
 }
